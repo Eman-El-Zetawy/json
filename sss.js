a0a2d642c3b15b921b340e38198b3d6c419eeb1d
Eman-El-Zetawy <dovprogrammer@gmail.com>
2019-10-24 09:01:50 -0700

    post

--- sss.js
+++ sss.js
@@ -1,43 +1,79 @@
-var zar=document.getElementById("zar");
-var info=document.getElementById("info");
+var get=document.getElementById("get");
+var post=document.getElementById("post");
+var pull=document.getElementById("pul");
+var dlt=document.getElementById("delete");
+var inf =document.getElementById("info");
 var fname=document.getElementById("n1");
+var lname=document.getElementById("n4");
+var gender=document.getElementById("n5");
 var id=document.getElementById("n2");
 var email=document.getElementById("n3");
 var p=document.getElementById("p");
-zar.addEventListener("click",f);
+get.addEventListener("click",f1);
+post.addEventListener("click",f2);
+pull.addEventListener("click",f3)
+ dlt.addEventListener("click",f4);
+const my = new Headers();
+   my.append('authorization' , 'Bearer xEpYEON0z20mHSOmx-0wVbYGqtNRAOwXAJfj') ; 
+   my.append('Content-Type','application/json'); 
 
-
-function f () 
-{var w ='' , a=[];
-
-const m =new Headers();
-   m.append('Authorization' , 'Bearer xEpYEON0z20mHSOmx-0wVbYGqtNRAOwXAJfj')  ; 
-   m.append('Content-Typy','application/json'); 
-
-   if(id.value !==''){
-       w='/'+id.value ;
-}
-   if(p.value !=='' ){
-    a.push('page='+p.value) ;
-   }
-if(fname.value !==''){
-    a.push('first_name='+ fname.value);
-}
-if(email.value !==''){
-a.push('email='+email.value) ;
-}
-
-  w = w+'?'+a.join('&&');
-  console.log(w);
-
+function f1 () 
+    {  var w ='' , a=[];
+      if(id.value !==''){ w='/'+id.value ; }
+        if(p.value !=='' ){  a.push('page='+p.value) ; }
+      if(fname.value !==''){ a.push('first_name='+ fname.value);}
+      if(lname.value !==''){ a.push('last_name='+ lname.value);}
+      if(email.value !==''){ a.push('email='+email.value) ; }
+      if(gender.value==1){ a.push("gender=male");}
+      if(gender.value==2){ a.push("gender=female");}
+      if(a.length !==0){ w = w+'?'+a.join('&&'); }
+      console.log(a);
+        console.log(w);
 fetch('https://gorest.co.in/public-api/users'+w , {
   method :'GET',
-  headers: m ,
-// body:JSON.stringify({ +'email='+email.value
-//  first_name:"yfghk",
-//  last_name:"hgll" ,
-//  gender:"female" ,
-//  email:"HLJKHJ@example.com"})
+  headers: my 
 }).then(response=>response.json()).then(data=>console.log(data));
-
 }
+
+function f2 (){
+  var l ; 
+  if(gender.value==1){ l="male";}
+      if(gender.value==2){ l="female";}
+     console.log(fname.value ,lname.value ,email.value , l)
+    fetch('https://gorest.co.in/public-api/users',{
+        method:'POST',
+        headers: my ,
+      body:JSON.stringify({
+        first_name:fname.value ,
+        last_name:lname.value,
+        email:email.value,
+          gender:l
+    })
+      }).then(response=>response.json()).then(data=>console.log(data));
+      }
+      function f3(){
+        var l='';
+        if(gender.value==1){ l="male";}
+        if(gender.value==2){ l="female";}
+        fetch('https://gorest.co.in/public-api/users', {
+            method :'PUL',
+            headers: my ,
+          first_name:fname.value ,
+          last_name:lname.value,
+          email:email.value,
+          gender:l
+          }).then(response=>response.json())
+          .then(data=>console.log(data));
+          
+    }
+function f4(){
+    var d='';
+    d ='/'+id.value;
+    fetch('https://gorest.co.in/public-api/users'+d, {
+        method :'DELETE',
+        headers: my 
+      }).then(response=>response.json())
+      .then(data=>console.log(data));
+}
+
+// inf.insertAdjacentHTML('beforeend',data) }
